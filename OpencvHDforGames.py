2c5e25e3af4afc7917875f408df027a53ac93dcc
Suraj <skbanavalikar2105@gmail.com>
2026-04-12 22:40:02 +0530

    Hand location detection

--- OpencvHDforGames.py
+++ OpencvHDforGames.py
@@ -4,24 +4,31 @@ import mediapipe as mp
 mp_draw = mp.solutions.drawing_utils
 mp_hand = mp.solutions.hands
 
-video = cv2.VideoCapture(0)  
+video = cv2.VideoCapture(0)
 
-with mp_hand.Hands(min_detection_confidence=0.5,min_tracking_confidence=0.5) as hands:
+with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
     while True:
-        ret,image = video.read()
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        image.flags.writeable=False                 # Makes the image read-only to improve performance while MediaPipe processes it (no need to modify pixels).
+        ret, image = video.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False  # Makes the image read-only to improve performance while MediaPipe processes it (no need to modify pixels).
         results = hands.process(image)
-        image.flags.writeable=True                  # Re-enables write access so we can draw on the image again.
-        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+        image.flags.writeable = (
+            True  # Re-enables write access so we can draw on the image again.
+        )
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
             for hand_landmark in results.multi_hand_landmarks:
-                mp_draw.draw_landmarks(image,hand_landmark,mp_hand.HAND_CONNECTIONS)
+                myHands = results.multi_hand_landmarks[0]
+                for id, lm in enumerate(hand_landmark.landmark):
+                    h, w, c = image.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    print(id, cx, cy)
+                mp_draw.draw_landmarks(image, hand_landmark, mp_hand.HAND_CONNECTIONS)
 
-        cv2.imshow("Frame",image)
+        cv2.imshow("Frame", image)
         k = cv2.waitKey(1)
-        if k==ord('q'):
+        if k == ord("q"):
             break
-    
+
 video.release()
 cv2.destroyAllWindows()
